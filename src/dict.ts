98abbd331f36086841d5934b31996e2f07575d0b
cckn <cckn.dev@gmail.com>
2021-01-12 22:17:11 +0900

    dict 추가

--- src/dict.ts
+++ src/dict.ts
@@ -1,7 +1,18 @@
-const dictionary: { lint: string; replace: string }[] = [];
-dictionary.push({ lint: "쫌", replace: "조금" });
-dictionary.push({ lint: "쪼끔", replace: "조금" });
-dictionary.push({ lint: "니", replace: "네" });
-dictionary.push({ lint: "니가", replace: "네가" });
+const dictionary: { lint: string; replace: string }[] = [
+  { lint: "쫌", replace: "조금" },
+  { lint: "쪼끔", replace: "조금" },
+  { lint: "쪼금", replace: "조금" },
+  { lint: "쪼끔씩", replace: "조금씩" },
+  { lint: "쪼금씩", replace: "조금씩" },
+  { lint: "니", replace: "네" },
+  { lint: "니가", replace: "네가" },
+  { lint: "니는", replace: "너는" },
+  { lint: "갖고", replace: "가지고" },
+  { lint: "그래갖고", replace: "그래가지고" },
+  { lint: "할라면은", replace: "하려면은" },
+  { lint: "할려면은", replace: "하려면은" },
+  { lint: "같애도", replace: "같아도" },
+  { lint: "같애서", replace: "같아서" },
+];
 
 export { dictionary };
